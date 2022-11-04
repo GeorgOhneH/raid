3f1231272942c4fd156d62f61210bc008062bd18
Georg <georg.schwan@vtxnet.ch>
2022-11-04 19:03:58 +0800

    working somewhat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use crate::matrix::Matrix;
+use crate::galois::Galois;
+
+pub mod galois;
+pub mod matrix;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut a = Galois::new(10);
+    let b = Galois::new(12);
+    let c = Galois::new(11);
+    let mut d = &mut a;
+    d -= Galois::new(8);
+    dbg!(d);
+    dbg!( Galois::new(10) - Galois::new(8));
+    dbg!(10 ^ 8);
+
+
+    let d = [Galois::new(3), Galois::new(5), Galois::new(8), Galois::new(10), Galois::new(15)];
+    let matrix = Matrix::<3, 5>::vandermonde();
+    let c = matrix.mul_vec(&d);
+    let mut rec_matrix = matrix.recovery_matrix(vec![1, 0, 3, 4, 2], vec![]);
+    println!("{:?}", rec_matrix);
+    println!("{:?}", matrix);
+    println!("{:?}", c);
+
+    let r = rec_matrix.gaussian_elimination(d);
+    println!("{:?}", r);
+
+
 }
---
+++ src/matrix.rs
@@ -0,0 +1,118 @@
+use crate::galois::Galois;
+use std::fmt::{Debug, Formatter};
+
+pub struct Matrix<const M: usize, const N: usize> {
+    data: [[Galois; N]; M],
+}
+
+impl<const M: usize, const N: usize> Debug for Matrix<M, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("[\n")?;
+        for row in &self.data {
+            Debug::fmt(row, f)?;
+            f.write_str("\n")?;
+        }
+        f.write_str("]")
+    }
+}
+
+impl<const N: usize> Matrix<N, N> {
+    pub fn gaussian_elimination(&mut self, mut vec: [Galois; N]) -> [Galois; N] {
+        let mut swap_history = vec![];
+        for m in 0..N {
+            // swapp if zero
+            if self.data[m][m] == Galois::zero() {
+                for m_below in m+1..N {
+                    if self.data[m_below][m] != Galois::zero() {
+                        self.data.swap(m, m_below);
+                        vec.swap(m, m_below);
+                        swap_history.push((m, m_below));
+                        break
+                    }
+                }
+            }
+
+            if self.data[m][m] == Galois::zero() {
+                println!("{:?}, {}", self, m);
+                panic!("Singular matrix")
+            }
+
+            if self.data[m][m] != Galois::one() {
+                let scale = Galois::one() / self.data[m][m];
+                for i in 0..N {
+                    self.data[m][i] *= scale;
+                }
+            }
+
+            for m_below in m+1..N {
+                if self.data[m_below][m] != Galois::zero() {
+                    let scale = self.data[m_below][m];
+                    for e in 0..N {
+                        self.data[m_below][e] -= scale * self.data[m][e]
+                    }
+                    vec[m_below] -= scale * vec[m]
+                }
+            }
+        }
+
+        for m in (0..N-1).rev() {
+            for c in m+1..N {
+                vec[m] -= vec[c] * self.data[m][c]
+            }
+        }
+
+        for (x, y) in swap_history.into_iter().rev() {
+            vec.swap(x, y)
+        }
+
+        vec
+    }
+}
+
+impl<const M: usize, const N: usize> Matrix<M, N> {
+    pub fn vandermonde() -> Self {
+        let data = core::array::from_fn(|m| core::array::from_fn(|n| Galois::new((n + 1) as u8).exp(m)));
+        Self {
+            data,
+        }
+    }
+
+    pub fn recovery_matrix(&self, mut ds: Vec<usize>, cs: Vec<usize>) -> Matrix<N, N> {
+        assert!(ds.len() + cs.len() <= N);
+
+        let mut x = 0;
+        while ds.len() + cs.len() != N {
+            if !ds.contains(&x) {
+                ds.push(x)
+            }
+            x += 1;
+        }
+        let data = core::array::from_fn(|m| {
+            if m < N - cs.len() {
+                core::array::from_fn(|n| {
+                    if ds[m] == n {
+                        Galois::one()
+                    } else {
+                        Galois::zero()
+                    }
+                })
+            } else {
+                self.data[cs[m-ds.len()]].clone()
+            }
+        });
+
+        Matrix::<N, N> {
+            data
+        }
+    }
+
+    pub fn mul_vec(&self, vec: &[Galois; N]) -> [Galois; M] {
+        let mut result = [Galois::zero(); M];
+        for (mut r, col) in result.iter_mut().zip(&self.data) {
+            for (m, v) in col.iter().zip(vec) {
+                r += m * v;
+            }
+        }
+        result
+    }
+}
